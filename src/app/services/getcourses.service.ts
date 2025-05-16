01f4423901d70b028af037920a07ab2fa4a37cb3
David Löwy <david.lowy@hotmail.com>
2025-05-16 09:53:06 +0200

    readme tillagd

--- src/app/services/getcourses.service.ts
+++ src/app/services/getcourses.service.ts
@@ -10,8 +10,9 @@ export class GetcoursesService {
 
   //egen notering: private innebär att variablen bara kan nås inom klassen.
 
-  // private url:string = "ramschema.json"; //Länkar till json i public-mappen, använt för att testa båda varianterna.
-  private url:string = "https://webbutveckling.miun.se/files/ramschema.json";
+//Länkar till json i public-mappen resp webb, använt för att testa båda varianterna.
+  private url:string = "ramschema.json"; 
+  // private url:string = "https://webbutveckling.miun.se/files/ramschema.json";
   
   constructor(private http: HttpClient) { } //Gör att http-client blir tillgänglig i denna klass (dependency injection)
 
