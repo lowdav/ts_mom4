7516505cc36b0714cbcc30b156fa12ef8347eedd
David Löwy <david.lowy@hotmail.com>
2025-05-16 09:31:19 +0200

    Sortering och filter fungerar

--- src/app/courselist/courselist.component.ts
+++ src/app/courselist/courselist.component.ts
@@ -12,15 +12,43 @@ import { FormsModule } from '@angular/forms';
 })
 export class CourselistComponent {
   courselist: Course[] = [];
+  searchTerm: string = "";
+  column: string = "";
 
 constructor(private getcoursesservice: GetcoursesService) {}
  
-//Prenumeration på den observable som skapades i getcourses. services.ts
+//Prenumeration på den observable som skapades i getcourses.services.ts
 ngOnInit() {
     this.getcoursesservice.getCourses().subscribe(data => {
       this.courselist = data;
     })
-
+  };
+//Sökning
+//Filtrera listan baserat på sökning i sökfältet
+filteredCourses(): Course[] {
+  //Om inget i sökfältet returnera hela listan
+  if(!this.searchTerm) {
+    return this.courselist;
   }
+
+  //Spara sökfrasen i term, som små bokstäver
+  const term = this.searchTerm.toLowerCase();
+  //Skicka tillbaka de kurser som inkluderar sökordet.
+  return this.courselist.filter(course =>
+    course.code.toLowerCase().includes(term) ||
+    course.coursename.toLowerCase().includes(term) ||
+    course.progression.toLowerCase().includes(term)
+  );
+}
+//Sortering
+//Parameter column måste vara en giltig egenskap i interfacet Course (key of)
+sortCourses(column: keyof Course) {
+  //vanlig sortering, stigande ordning.
+  // små bokstäver används eftersom js gör om bokstäver till numeriskt värde och det skiljer mellan stora och små 
+  this.courselist.sort((a, b) => 
+    a[column].toLowerCase().localeCompare(b[column].toLowerCase())
+  );
+}
+
 }
 
